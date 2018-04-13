be0273b171e62765ff1d69759b8a6852ebfbb291
hitechstudios <38322663+hitechstudios@users.noreply.github.com>
2018-04-13 10:22:19 +0200

    Update connessione.php

--- connessione.php
+++ connessione.php
@@ -1,7 +1,7 @@
 <?php
 
   $pw="";
-  $connessione = mysqli_connect('localhost','root',$pw);
+  $connessione = mysqli_connect('localhost','root');
   
  	mysqli_select_db($connessione, 'my_hitechstudios') or die('errore');
 
