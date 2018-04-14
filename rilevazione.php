e46503264271d84badd79167bffb8e436066d2c4
hitechstudios <38322663+hitechstudios@users.noreply.github.com>
2018-04-14 09:01:20 +0200

    Update rilevazione.php

--- rilevazione.php
+++ rilevazione.php
@@ -28,7 +28,7 @@ echo htmlspecialchars($stringaSplitted, ENT_QUOTES, 'UTF-8');
 
 
 $inserisciRiv = mysqli_prepare($connessione, 'INSERT INTO RaccoltaDati (Fk_Sensore, Data, Rilevazione, Dettagli)
-				VALUES ('?','?','?','?') ');
+				VALUES (?,?,?,?) ');
                 
  mysqli_stmt_bind_param($inserisciRiv, 'isss', $Fk_Sensore, NOW(), $Rilevazione, $Dettagli);
 	mysqli_stmt_execute($InserisciRiv);             
